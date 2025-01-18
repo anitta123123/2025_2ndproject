74c61ac81c9e8f3ff5374592988d338851daccfd
anitta123123 <anittatom18@gmail.com>
2025-01-18 15:58:52 +0530

    codes from Y to develop branch

---
+++ src/main/java/FrameworkMukesh/FrameworkMukesh/YFromAsian.java
@@ -0,0 +1,14 @@
+package FrameworkMukesh.FrameworkMukesh;
+
+public class YFromAsian {
+	
+	public void test() {
+	
+	System.out.println( "Hello World!test3454" );
+	System.out.println( "Hello World!" );
+	System.out.println( "Hello World!" );
+	
+	
+
+}
+}
