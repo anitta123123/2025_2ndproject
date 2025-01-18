57b12339d0b02d0bc4825cee976bf1dfe07fc19c
anitta123123 <anittatom18@gmail.com>
2025-01-18 09:39:35 +0530

    commit from X person

---
+++ src/main/java/FrameworkMukesh/FrameworkMukesh/XCommit.java
@@ -0,0 +1,12 @@
+package FrameworkMukesh.FrameworkMukesh;
+
+public class XCommit {
+
+	public void first() {
+		
+		System.out.println("ist line from X");
+		System.out.println("anitta");
+
+
+	}
+}
